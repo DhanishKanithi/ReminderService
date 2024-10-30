0a7055d15173b2c8b1a209e98ad6745eef397bf6
Dhanish Kanithi <dhanishkanithi@gmail.com>
2024-10-30 13:13:47 +0530

    Added notification cron

--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,8 +1,12 @@
 const sender = require('../config/emailConfig');
 
+const TicketRepository = require('../repository/ticket-repository');
+
+const repo = new TicketRepository();
+
 const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) => {
     try {
-        const response = sender.sendMail({
+        const response = await sender.sendMail({
             from: mailFrom,
             to: mailTo,
             subject: mailSubject,
@@ -11,10 +15,40 @@ const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) => {
         console.log(response);
     } catch (error) {
         console.log(error);
+    }       
+}
+
+const fetchPendingEmails = async (timestamp) => {
+    try {    
+        const response = await repo.get({status: "PENDING"});
+        return response;
+    } catch (error) {
+        console.log(error);
     }
-        
+}
+
+const updateTicket = async (ticketId, data) => {
+    try {    
+        const response = await repo.update(ticketId, data);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const createNotification = async (data) => {
+    try {
+        const response = await repo.create(data);
+        return response;
+    } catch (error) {
+        console.log(error);
+
+    }
 }
 
 module.exports = {
-    sendBasicEmail
+    sendBasicEmail,
+    fetchPendingEmails,
+    createNotification,
+    updateTicket
 }
